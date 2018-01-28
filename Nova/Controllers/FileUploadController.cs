ee9f14308746751e656bab3bbc5b38c855df87ff
redxman <108541768@qq.com>
2018-01-28 21:21:03 +0800

    添加项目文件。

---
+++ Nova/Controllers/FileUploadController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Nova.Helpers;
+using Nova.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Nova.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        FilesHelper filesHelper;
+        String tempPath = "~/somefiles/";
+        String serverMapPath = "Files/somefiles/";
+        private string StorageRoot
+        {
+            //get { return Path.Combine(HostingEnvironment.MapPath(serverMapPath)); }
+            get { return Path.Combine(_hostingEnvironment.WebRootPath, serverMapPath); }
+        }
+        private string UrlBase = "/Files/somefiles/";
+        String DeleteURL = "/FileUpload/DeleteFile/?file=";
+        String DeleteType = "GET";
+        public FileUploadController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            filesHelper = new FilesHelper(DeleteURL, DeleteType, StorageRoot, UrlBase, tempPath, serverMapPath);
+         }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Show()
+        {
+            JsonFiles ListOfFiles = filesHelper.GetFileList();
+            var model = new FilesViewModel()
+            {
+                Files = ListOfFiles.files
+            };
+
+            return View(model);
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Upload()
+        {
+            var resultList = new List<ViewDataUploadFilesResult>();
+
+            var CurrentContext = HttpContext;
+
+            filesHelper.UploadAndShowResults(CurrentContext, resultList);
+            JsonFiles files = new JsonFiles(resultList);
+
+            bool isEmpty = !resultList.Any();
+            if (isEmpty)
+            {
+                return Json("Error ");
+            }
+            else
+            {
+                return Json(files);
+            }
+        }
+        public JsonResult GetFileList()
+        {
+            var list = filesHelper.GetFileList();
+            return Json(list);
+        }
+        [HttpGet]
+        public JsonResult DeleteFile(string file)
+        {
+            filesHelper.DeleteFile(file);
+            return Json("OK");
+        }
+    }
+}
---
+++ Nova/Models/FilesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Nova.Helpers;
+
+namespace Nova.Models
+{
+    public class FilesViewModel
+    {
+        public ViewDataUploadFilesResult[] Files { get; set; }
+
+    }
+}
